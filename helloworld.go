d0dd8f55b1cfe1213cd291fb26c92c330521ea48
Gaston2018 <gaston_1987@live.com.ar>
2018-06-08 00:23:57 -0300

    helloworld web

--- helloworld.go
+++ helloworld.go
@@ -3,10 +3,14 @@ package main
 import(
     "net/http"
     "io"
-
 )
 
 func main()
 {
     http.ListenAndServer("http://ec2-18-219-147-32.us-east-2.compute.amazonaws.com/",nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+
+    io.WriteString(w, "Hola Mundo")
 }
