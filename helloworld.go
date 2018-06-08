65af60238fef224091666932d637e738ebadb31a
Gaston2018 <gaston_1987@live.com.ar>
2018-06-08 00:35:03 -0300

    helloworld web

--- helloworld.go
+++ helloworld.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
 
 func main() {
+    http.HandleFunc("/holamundo", func(w http.ResponseWriter, r *http.Request){
+            io.WriteString("Holllaaa!!!")
+    }
 	http.HandleFunc("/", handler)
 	http.ListenAndServe("http://ec2-18-219-147-32.us-east-2.compute.amazonaws.com/", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("Hay una nueva petición")
 	io.WriteString(w, "Hola Mundo")
 }
