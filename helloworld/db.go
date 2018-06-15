788343583acfa80686e5acbeb8a98d27a78fad89
Gaston2018 <gaston_1987@live.com.ar>
2018-06-15 00:49:54 -0300

    ultimos cambios

--- helloworld/db.go
+++ helloworld/db.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func basemain() {
+func main2() {
 	fmt.Println("Go MySQL")
 
 	db, err := sql.Open("mysql", "root:1234@tcp(localhost:3306)/tallervi")
@@ -18,7 +18,7 @@ func basemain() {
 
 	defer db.Close()
 
-	insert, err := db.Query("INSERT INTO profesionales VALUES ('Gaston', 'Rivainera', '20893', 'veterinario')")
+	insert, err := db.Query("INSERT INTO profesionales VALUES ('Morgan', 'Rivainera', '20894', 'paciente')")
 
 	if err != nil {
 		panic(err.Error())
