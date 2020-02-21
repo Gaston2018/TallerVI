3e2a6041d0ea4e552e727513d3431a1500da3676
aaron <aaronsaidalgaba@gmail.com>
2020-02-21 11:40:39 -0300

    Back Subido a heroku

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"TallerVI/controllers"
+	"TallerVI/driver"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-
-	"TallerVI/controllers"
-	"TallerVI/driver"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/subosito/gotenv"
@@ -23,8 +23,13 @@ func main() {
 
 	db = driver.ConnectDB()
 	controller := controllers.Controller{}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 	ruta := mux.NewRouter()
 
+	ruta.HandleFunc("/", index).Methods("GET")
 	ruta.HandleFunc("/turnos", controller.Turnos(db)).Methods("Get")
 	ruta.HandleFunc("/turno/{id}", controller.Detalle(db)).Methods("Get")
 	ruta.HandleFunc("/turnos", controller.NuevoTurno(db)).Methods("Post")
@@ -32,5 +37,14 @@ func main() {
 	ruta.HandleFunc("/turno/{id}", controller.DelTurno(db)).Methods("Delete")
 
 	fmt.Println("usar el puerto 8000")
-	log.Fatal(http.ListenAndServe(":8000", ruta))
+	log.Fatal(http.ListenAndServe(":"+port, ruta))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "bienvenidos")
+}
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
