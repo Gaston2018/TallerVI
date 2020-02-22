c91dd53b3b10ac2327823fe20c2adfd7343ae091
gaston2018 <gaston_1987@live.com.ar>
2020-02-22 15:54:23 -0300

    changes

--- tallervi/src/App.js
+++ tallervi/src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
+//import background from '..public/img/Index.jpg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <header class="header">
+        <div class="menu">
+                <img src="../public/img/logo.png" alt="logo" width="120" class="logo"/>
+                <nav class="nav">
+                    <ul>
+                        <li><a href="#">Solicitar turno</a></li>
+                        <li><a href="#">Nuevo paciente</a></li>
+                        <li><a href="#">Nuevo veterinario</a></li>
+                    </ul>
+                </nav>
+        </div>
+
+    </header>
+    <div id = 'contenedor'>
+      <table id='turnos'>
+        <thead>
+          <td>mascota</td>
+        </thead>
+        </table> 
+
+    </div>
+    <section>
+        <article>
+
+        </article>
+    </section>
+    <aside>
+
+    </aside>
+    <footer>
+
+    </footer>
     </div>
   );
 }
