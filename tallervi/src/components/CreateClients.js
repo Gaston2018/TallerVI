acee11557b43c4ef04ce635f5f2de83ee878f115
gaston2018 <gaston_1987@live.com.ar>
2020-02-25 15:17:13 -0300

    update frontend

---
+++ tallervi/src/components/CreateClients.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateClients extends Component {
+    render() {
+        return (
+            <div>
+                CREAR CLIENTE
+            </div>
+        )
+    }
+}
